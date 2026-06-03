22d05f3f83eb98de57b33970b2a93bc7902f9cc9
Altaryu <jeffgar4@gmail.com>
2026-06-03 13:53:16 -0400

    Added extract_markdown_images and extract_markdown_links

---
+++ src/test_extract_markdown_images.py
@@ -0,0 +1,17 @@
+import unittest
+from extract_markdown_images import extract_markdown_images, extract_markdown_links
+
+def test_extract_markdown_images(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+def test_extract_markdown_links(self):
+    matches=extract_markdown_links(
+        "This text with a link [link](https://www.test.com)"
+    )
+    self.assertListEqual([("link", "https://www.test.com")], matches)
+
+if __name__ == "__main__":
+    unittest.main()
