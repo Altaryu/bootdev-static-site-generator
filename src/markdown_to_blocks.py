ddd31edffbbcd95f0490a4cc71376524136f953f
Altaryu <jeffgar4@gmail.com>
2026-06-07 14:33:03 -0400

    Added block_to_block_type function

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
     result=markdown.split("\n\n")
@@ -7,3 +16,17 @@ def markdown_to_blocks(markdown):
         else:
             result.pop(i)
     return result
+
+def block_to_block_type(markdown):
+    if "# " in markdown or "## " in markdown or "### " in markdown or "#### " in markdown or "##### " in markdown or "###### " in markdown:
+        return BlockType.HEADING
+    elif "```\n" in markdown:
+        return BlockType.CODE
+    elif ">" in markdown and "<" in markdown:
+        return BlockType.QUOTE
+    elif "- " in markdown:
+        return BlockType.UNORDERED_LIST
+    elif "1. " in markdown:
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_to_blocks import markdown_to_blocks
+from markdown_to_blocks import markdown_to_blocks, block_to_block_type
 
 def test_markdown_to_blocks(self):
     md = """
@@ -20,6 +20,16 @@ def test_markdown_to_blocks(self):
             "- This is a list\n- with items",
         ],
         )
+    
+def test_block_to_block_type_equal(self):
+        block = block_to_block_type("This is a paragraph")
+        block2 = block_to_block_type("This is also a paragraph")
+        self.assertEqual(block, block2)
+
+def test_block_to_block_type_inequal(self):
+        block = block_to_block_type("This is a paragraph")
+        block2 = block_to_block_type("# This is a heading")
+        self.assertNotEqual(block, block2)
 
 if __name__ == "__main__":
     unittest.main()
